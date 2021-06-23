dc40fb51ea4aaf5002b6d44c06a6d4480bef580b
Ins <insayy@gmail.com>
2021-06-23 19:29:12 +0200

    actions

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from "enzyme"
+import CartView from './views/CartView';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Header from './components/header/Header';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new Adapter() });
+
+// test('renders learn react link', () => {
+//   render(<CartView />);
+//   const linkElement = do;
+//   expect(linkElement).toBeInTheDocument();
+//   expect(<div></div>).toBeInTheDOM;
+// });
+
+let wrapper = shallow(<Header />)
+
+test("render correctly", () => {
+    expect(wrapper).toMatchSnapshot();
+})
